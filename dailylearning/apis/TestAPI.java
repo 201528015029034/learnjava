d77db3ba257be9b473c9b040fdf8b8d3e9a67886
zzsilence <1536918768@qq.com>
2017-03-14 01:16:21 +0800

    project init

---
+++ dailylearning/apis/TestAPI.java
@@ -0,0 +1,17 @@
+package apis;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by test on 2017/3/13.
+ */
+public class TestAPI {
+
+
+    @Test
+    public void testCharacter() {
+        //LeetCode Num 10 atoi  简化判断
+        System.out.println(Character.isLetterOrDigit('1'));
+        System.out.println(Character.toLowerCase('9'));
+    }
+}
