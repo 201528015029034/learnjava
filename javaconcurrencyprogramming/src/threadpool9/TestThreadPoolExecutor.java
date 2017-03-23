efb2dccd0d0a03989918a9f792becd3f480be524
silence <1499191187@qq.com>
2017-03-23 23:39:50 +0800

    nothing changes

--- javaconcurrencyprogramming/src/threadpool9/TestThreadPoolExecutor.java
+++ javaconcurrencyprogramming/src/threadpool9/TestThreadPoolExecutor.java
@@ -14,7 +14,8 @@ public class TestThreadPoolExecutor {
      * 这里需要注意ThreadPoolExecutor各个参数的含义：
      * new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, milliseconds, runnableTaskQueue, handler)
      * corePoolSize:核心线程的数量，当线程池的线程的数量小于corePoolSize时，每来一个任务就会新建一个线程运行任务，
-     *              直到线程数量达到corePoolSize。也可以通过线程池的prestartAllCoreThreads()预先创建并启动全部线程。
+     *              不将任务放入任务队列，直到线程数量达到corePoolSize。也可以通过线程池的prestartAllCoreThreads()
+     *              预先创建并启动全部线程。
      * maximumPoolSize: 线程池支持的最大线程的数量，注意这里不是限制任务的数量，而是限制工作者线程Worker的数量。
      *                  如果任务队列满了，并且已创建的线程数小于maximumPoolSize,那线程池就会新建工作者线程，那也
      *                  就是说这个参数对于无界的任务队列没有效果。
