39bcf13dfff5e4f05665390a58013775c02bde00
zzsilence <1536918768@qq.com>
2017-04-23 23:27:00 +0800

    测试stack api

--- dailylearning/src/apis/TestAPI.java
+++ dailylearning/src/apis/TestAPI.java
@@ -2,16 +2,27 @@ package apis;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+
 /**
  * Created by test on 2017/3/13.
  */
 public class TestAPI {
 
 
+    //测试Character的isLetterOrDigit和toLowerCase
     @Test
     public void testCharacter() {
         //LeetCode Num 10 atoi  简化判断
         System.out.println(Character.isLetterOrDigit('1'));
         System.out.println(Character.toLowerCase('9'));
+    }
+
+    @Test
+    public void testLinkedListStack() {
+        LinkedList<Integer> stack = new LinkedList<Integer>();
+        stack.push(1);
+        stack.push(2);
+        System.out.println(stack.pop());
     }
 }
